6e302510216ed2d25b8de2b3a94e5ab0320730e3
Shubh595 <42946252+Shubh595@users.noreply.github.com>
2020-08-30 23:26:00 +0530

    Add files via upload

---
+++ React Final Project/src/Components/resume.js
@@ -0,0 +1,163 @@
+import React, {
+    Component
+} from 'react';
+
+import './styles.css';
+class Resume extends Component {
+    render() {
+        return ( <
+            section className = "resume" >
+
+            <
+            h1 > Resume < /h1>
+
+
+            <
+            div class = "entry" >
+            <
+            h2 > Qualification < /h2> <
+            div class = "content" >
+            <
+            h3 > Year: 2017 - 2021 < /h3> <
+            p > Btech - Information Technology < br / >
+            <
+            em > Lovely Professional University < /em></p >
+            <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Year: 2014 - 2016 < /h3> <
+            p > XII(BSEB) < br / >
+            <
+            em > Sant Baba Bhag Singh International School < /em></p >
+            <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Year: 2014 - 2015 < /h3> <
+            p > X(CBSE) < br / >
+            <
+            em > St. Joseph Public School < /em></p >
+            <
+            /div> <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > Projects < /h2> <
+            div class = "content space" >
+            <
+            h3 > May 2019 - June 2019 < /h3> <
+            p > Sports Car Website < br / >
+            <
+            em > EME Technoligies, Mohali < /em></p >
+            <
+            ul class = "folio" >
+            <
+            li > Developed a website using front - end web development technologies. < /li> <
+            br / >
+            <
+            li > Design idea was taken from Bugatti.com. < /li> <
+            /ul> <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Oct 2018 - Nov 2018 < /h3> <
+            p > Restaurant Management System < br / >
+            <
+            em > Lovely Professional University < /em></p >
+            <
+            ul class = "folio" >
+            <
+            li > Group Website Project using HTML / CSS and JavaScript < /li> <
+            br / >
+            <
+            li > Aim of this project was to delve deep into the frontend technologies < /li> <
+            /ul> <
+            /div> <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > Certifications < /h2> <
+            div class = "content" >
+            <
+            ul class = "folio" >
+            <
+            li > Certificate
+            for participation in Renovation Tech.Robotics and Aero Modelling < /li> <
+            br / >
+            <
+            li > Certification
+            for completion of project in machine learning by workshop organised by Techinest association with Aavriti 19 ' IIT, Mumbai.</li> <
+            br / >
+            <
+            li > Certificate of best website designer in summer training by EME Technologies. <
+            /li> <
+            br / >
+            <
+            li >
+            Certificate of Advanced Web Concepts by Udemy <
+            /li> <
+            /ul> <
+            /div>
+
+            <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > SKILLS < /h2> <
+            div class = "content" >
+            <
+            h3 > Software Knowledge < /h3> <
+            ul class = "skills" >
+            <
+            li > Photoshop < /li> <
+            li > Microsoft Office < /li> <
+            li > GIT < /li> <
+            /ul> <
+            /div> <
+            div class = "content" >
+            <
+            h3 > Languages < /h3> <
+            ul class = "skills" >
+            <
+            li > CSS / XHTML < /li> <
+            li > JavaScript / Jquery < /li> <
+            li > React < /li> <
+            li > Sass < /li> <
+            /ul> <
+            /div> <
+            /div>
+
+            <
+            div class = "entry" >
+            <
+            h2 > Personal Details < /h2>
+
+            <
+            p > Date Of Birth: 24 st Feb, 1998 < /p> <
+            p > Nationality: Indian < /p> <
+            p > Address: Barh, Distt.Patna < /p>
+
+            <
+            /div>
+
+
+            
+
+
+
+
+
+            <
+            /section>
+        );
+    }
+}
+
+export default Resume;
